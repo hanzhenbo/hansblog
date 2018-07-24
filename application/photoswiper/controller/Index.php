71dd8aad418ea8c279150adb687480819e54135c
hzb <1017897955@qq.com>
2018-07-24 16:01:29 +0800

    photoswiper初步上传展示

--- application/photoswiper/controller/Index.php
+++ application/photoswiper/controller/Index.php
@@ -8,11 +8,44 @@
 namespace app\photoswiper\controller;
 
 use controller\BasicAdmin;
+use think\Db;
 
 class Index extends BasicAdmin
 {
+    public $table = 'hans_photos';
+
     public function index()
     {
-        return $this->fetch();
+        $db = Db::name('hans_photos');
+        return $this->_list($db);
+    }
+
+    /**
+     * 批量上传照片
+     */
+    public function addphoto(){
+        if ($this->request->isGet()){
+            return $this->fetch();
+        }else{
+            $post = $this->request->post();
+            $insert = array();
+            $url = explode('|',$post['url']);
+            $check = Db::name($this->table)->whereIn('url',$url)->select();
+            if ($check){
+                return $this->error('照片已存在');
+            }
+            foreach($url as $key=>$value){
+                $insert['classify'] = '1';
+                $insert['url'] = $value;
+                $insertall[] = $insert;
+            }
+            $res = Db::name('hans_photos')->insertAll($insertall);
+            list($base, $spm, $url) = [url('@admin'), $this->request->get('spm'), url('photoswiper/index/addphoto')];
+            if ($res){
+                $this->success('上传成功',"{$base}#{$url}?spm={$spm}");
+            }else{
+                $this->error('上传失败');
+            }
+        }
     }
 }
